96a2d2487631d4c84a5471d43fb62e642f733c86
p000059 <marceloacad@gmail.com>
2019-07-16 10:34:57 -0300

    SP/spUpdateRegistro-If-Else.sql

---
+++ MySQL/SP/spUpdateRegistro-If-Else.sql
@@ -0,0 +1,19 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spAtualizartblCodigo`(
+		in	upd_pkCodigo 			int,
+        in	upd_nrCodigoPatrimonio 	varchar(20),
+        in	upd_srCodigoSerie		varchar(50)
+)
+BEGIN
+		 if(((upd_pkCodigo > 0)and(upd_pkCodigo != ''))and((upd_nrCodigoPatrimonio != '')and(upd_srCodigoSerie != '')))then
+         #Para que o update aconteça, o pk precisa ser maior que zero e diferente de zero e com as variáveis de entrada diferentes de nulo.
+			update 	
+					bdestoque.tblCodigo
+			   set 	
+					nrCodigoPatrimonio = upd_nrCodigoPatrimonio,
+					srCodigoSerie 	   = upd_srCodigoSerie
+		     where		
+					pkCodigo = upd_pkCodigo;         
+         else
+				select 'O número de patrimônio e o código de série deve ser fornecidos!' as Atenção;
+         end if;
+END
