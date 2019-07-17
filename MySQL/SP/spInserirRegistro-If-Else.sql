c6dae8ae417474f1b9ceea97c6f5da62c702e68b
p000059 <marceloacad@gmail.com>
2019-07-17 15:12:12 -0300

    SP/spInserirRegistro-If-Else.sql

--- MySQL/SP/spInserirRegistro-If-Else.sql
+++ MySQL/SP/spInserirRegistro-If-Else.sql
@@ -1,20 +1,28 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spInsereCodigo`(
-	in	p_nrCodigoPatrimonio		varchar(20),
-    in	p_srCodigoSerie				varchar(50)    
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spInsereInsumos`(
+		
+        out	id_pkInsumoGerado 	int,
+        in	p_nmInsumos			varchar(100),
+		in	p_nrInsumos			varchar(50)        
 )
 BEGIN
-	if((p_nrCodigoPatrimonio != '')&&(p_srCodigoSerie != '')) then
+        declare msg varchar(50);
+        set msg = 'Todos os campos devem ser preenchidos.';
         
-		INSERT INTO 
-			bdestoque.tblCodigo (
-									nrCodigoPatrimonio,
-									srCodigoSerie			
-								) 
-			 VALUES	(
-						p_nrCodigoPatrimonio,
-						p_srCodigoSerie					
-					);
-	else
-			select 'O código do patrimônio e código de série devem ser fornecidos!' as Atenção;
-	end if;
+        if(((p_nmInsumos > 0)and(p_nmInsumos != ''))and
+		   ((p_nrInsumos > 0)and(p_nrInsumos != '')))then
+        
+			insert into bdestoque.tblInsumos(
+							nmInsumos,
+							nrInsumos
+						)
+				 values	(
+							p_nmInsumos,
+							p_nrInsumos
+						);
+				
+                    set id_pkInsumoGerado = last_insert_id();                    
+        else
+                select msg;
+                #set msg_Retorno = msg;
+        end if; 
 END
