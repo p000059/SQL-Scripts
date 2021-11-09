dbc3695617ee4204e181bc101428481bb708569c
p000059 <52934496+p000059@users.noreply.github.com>
2021-11-09 01:40:19 -0300

    2020110914000

--- PostgreSQL/create_tblPessoa.sql
+++ PostgreSQL/create_tblPessoa.sql
@@ -1,52 +1,56 @@
 CREATE TABLE tb_TypePerson
 (
-	Id_TypePerson	serial not null primary key,
-	nm_TypePerson	varchar(30),
-	ds_TypePerson	varchar(100),
-	st_TypePerson	bit
+	id_TypePerson	serial not null unique primary key,
+	nm_TypePerson	char(30) not null,
+	ds_TypePerson	char(100) not null,
+	st_TypePerson	bit not null
 );
 
 CREATE TABLE tb_Person
 (
-  	Id_Person	SERIAL NOT NULL primary key,
-  	nm_Person	varchar(100),
-  	dt_Person	timestamp default current_timestamp,
-	Id_TypePerson int,
-  	st_Person boolean,
+  	id_Person		SERIAL NOT NULL unique primary key,
+  	nm_Person		varchar(100),
+  	dt_Person		char(14),
+	id_TypePerson 	int not null,
+  	st_Person 		bit not null,
 	
-	foreign key (Id_TypePerson) references tb_TypePerson (Id_TypePerson)
+	foreign key (id_TypePerson) references tb_TypePerson (id_TypePerson)
 );
 
 CREATE TABLE tb_TypeUser
 (
-	Id_TypeUser	serial primary key not null,
-	nm_TypeUser	varchar(20),
-	ds_TypeUser	varchar(100),
-	dt_TypeUser	timestamp default current_timestamp,
-	st_TypeUser	bit
+	id_TypeUser	serial not null unique primary key,
+	nm_TypeUser	char(20),
+	ds_TypeUser	varchar(150),
+	dt_TypeUser	char(14),
+	st_TypeUser	bit not null
 );
 
 CREATE TABLE tb_User
 (
-	Id_User	serial primary key NOT NULL,
-	cd_User	varchar (7) unique,
-	pw_User	character (30) unique,
-	dt_User timestamp DEFAULT current_timestamp,
-	Id_TypeUser int,
-	st_User bit,
+	id_User		serial not null unique primary key,
+	rg_User		char (8) unique,
+	ds_User		varchar (150) unique,
+	dt_User 	char(14),
+	id_TypeUser int not null,
+	st_User 	bit not null,
 	
-	foreign key (Id_TypeUser) references tb_TypeUser (Id_TypeUser)
+	foreign key (id_TypeUser) references tb_TypeUser (id_TypeUser)
 );
 
 CREATE TABLE rl_PersonUser
 (
-	Id_Person 	int,
-	Id_User		int,
+	id_Person 	int not null,
+	id_User		int not null,
 	
-	foreign key (Id_Person) references tb_Person (Id_Person),
-	foreign key (Id_User) references tb_User (Id_User)
-)
+	foreign key (id_Person) references tb_Person (id_Person),
+	foreign key (id_User) references tb_User (id_User)
+);
 
+--Esse bloco vai preencher a tabela tipo de pessoa.
+insert into tb_TypePerson(nm_TypePerson, ds_TypePerson, st_typeperson)
+values('Pessoa Natural', 'Popularmente conhecido com pessoa física.', '1');
 
-
+insert into tb_TypePerson(nm_TypePerson, ds_TypePerson, st_TypePerson)
+values('Pessoa Jurídica', 'Conhecido como Pessoa Jurídica', '1');
 
