2cdcaddd76a9931f85ae8d2a0a3a3ab24721956e
p000059 <52934496+p000059@users.noreply.github.com>
2022-06-03 00:07:38 -0300

    CREATA DATABASE DBADM

--- PostgreSQL/create_tblPessoa.sql
+++ PostgreSQL/create_tblPessoa.sql
@@ -1,56 +1,107 @@
-CREATE TABLE tb_TypePerson
+CREATE DATABASE DBADM
+
+CREATE TABLE tb_status
 (
-	id_TypePerson	serial not null unique primary key,
-	nm_TypePerson	char(30) not null,
-	ds_TypePerson	char(100) not null,
-	st_TypePerson	bit not null
+	id_status SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_status VARCHAR(2) NOT NULL,
+	nm_status VARCHAR(50) NOT NULL,
+	st_status BOOLEAN NOT NULL,
+	ds_status VARCHAR(200)	
 );
 
-CREATE TABLE tb_Person
+CREATE TABLE tb_doc
 (
-  	id_Person		SERIAL NOT NULL unique primary key,
-  	nm_Person		varchar(100),
-  	dt_Person		char(14),
-	id_TypePerson 	int not null,
-  	st_Person 		bit not null,
+	id_doc SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	isrg_doc VARCHAR(12),
+	cpfcnpj_doc VARCHAR(14)
+);
+
+CREATE TABLE tb_sex
+(
+	id_sex SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	nm_sex VARCHAR(20),
+	ds_sex VARCHAR(100)
+);
+
+-- domain table type person
+CREATE TABLE tb_typeperson
+(
+	id_typeperson	SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	nm_typeperson	VARCHAR(30) NOT NULL,
+	ds_typeperson	VARCHAR(100) NOT NULL	
+);
+
+CREATE TABLE tb_user
+(
+	id_user		SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_user		CHAR(7) NOT NULL,
+	ds_user		VARCHAR(150) NOT NULL,
+	dt_user 	VARCHAR(14) NOT NULL,
+	id_status	INT NOT NULL,
 	
-	foreign key (id_TypePerson) references tb_TypePerson (id_TypePerson)
+	FOREIGN KEY (id_status) REFERENCES tb_status(id_status)
 );
 
-CREATE TABLE tb_TypeUser
+CREATE TABLE tb_typeuser
 (
-	id_TypeUser	serial not null unique primary key,
-	nm_TypeUser	char(20),
-	ds_TypeUser	varchar(150),
-	dt_TypeUser	char(14),
-	st_TypeUser	bit not null
-);
-
-CREATE TABLE tb_User
-(
-	id_User		serial not null unique primary key,
-	rg_User		char (8) unique,
-	ds_User		varchar (150) unique,
-	dt_User 	char(14),
-	id_TypeUser int not null,
-	st_User 	bit not null,
+	id_typeuser	SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_typeuser VARCHAR(20) NOT NULL,
+	nm_typeuser	VARCHAR(20) NOT NULL,
+	ds_typeuser	VARCHAR(200) NOT NULL,
+	dt_typeuser	VARCHAR(14) NOT NULL,
+	id_status	INT NOT NULL,
 	
-	foreign key (id_TypeUser) references tb_TypeUser (id_TypeUser)
+	FOREIGN KEY (id_status) REFERENCES tb_status(id_status)
 );
 
-CREATE TABLE rl_PersonUser
+CREATE TABLE tb_person
 (
-	id_Person 	int not null,
-	id_User		int not null,
+  	id_person		SERIAL NOT NULL UNIQUE PRIMARY KEY,
+  	nm_person		VARCHAR(100) NOT NULL,
+  	dt_person		VARCHAR(14) NOT NULL,
+	id_typeperson 	INT NOT NULL,
+	id_doc			INT NOT NULL,
+	id_sex			INT NOT NULL,
 	
-	foreign key (id_Person) references tb_Person (id_Person),
-	foreign key (id_User) references tb_User (id_User)
+	FOREIGN KEY (id_typeperson) REFERENCES tb_typeperson(id_typeperson),
+	FOREIGN KEY (id_doc) REFERENCES tb_doc(id_doc),
+	FOREIGN KEY (id_sex) REFERENCES tb_sex(id_sex)
+);
+
+CREATE TABLE rl_personuser
+(
+	id_person 	INT NOT NULL,
+	id_user		INT NOT NULL,
+	
+	FOREIGN KEY (id_person) REFERENCES tb_person (id_person),
+	FOREIGN KEY (id_user) REFERENCES tb_user (id_user)
 );
 
 --Esse bloco vai preencher a tabela tipo de pessoa.
-insert into tb_TypePerson(nm_TypePerson, ds_TypePerson, st_typeperson)
-values('Pessoa Natural', 'Popularmente conhecido com pessoa física.', '1');
+insert into tb_typeperson(nm_typeperson, ds_typeperson)
+values('Pessoa Física', 'Pessoa natural com um registro de RG ou CPF.');
+insert into tb_typeperson(nm_typeperson, ds_typeperson)
+values('Pessoa Jurídica', 'Pessoa criada juridicamente como empresa.');
 
-insert into tb_TypePerson(nm_TypePerson, ds_TypePerson, st_TypePerson)
-values('Pessoa Jurídica', 'Conhecido como Pessoa Jurídica', '1');
+--Esse bloco vai preencher a tabela tipo de usuário.
+INSERT INTO tb_typeuser(co_typeuser, nm_typeuser, ds_typeuser, dt_typeuser, st_typeuser, ds_sttypeuser)
+VALUES('101','Administrador', 'Faz alterações administrativas na base de dados para todas funcionalidades do sistema.', '01022022124000',1,'Ativo');
 
+INSERT INTO tb_typeuser(co_typeuser, nm_typeuser, ds_typeuser, dt_typeuser, st_typeuser, ds_sttypeuser)
+VALUES('102','Administrador Local', 'Faz alterações no ambiente local e tem acesso a várias funcionalidades do sistema.', '02022022123000',1,'Ativo');
+
+INSERT INTO tb_typeuser(co_typeuser, nm_typeuser, ds_typeuser, dt_typeuser, st_typeuser, ds_sttypeuser)
+VALUES('103','Usuário Avançado', 'Faz consultas em a todas funcionalidades do sistema', '03022022113000',1,'Ativo');
+
+INSERT INTO tb_typeuser(co_typeuser, nm_typeuser, ds_typeuser, dt_typeuser, st_typeuser, ds_sttypeuser)
+VALUES('104','Usuário local', 'Faz consultas no ambiente local.', '04022022113000',0,'Inativo');
+
+--Esse bloco vai preencher a tabela status
+INSERT INTO tb_status(co_status, nm_status, st_status, ds_status)
+VALUES('1','Ativo',true,'Usuário de sistema em atividade.');
+
+INSERT INTO tb_status(co_status, nm_status, st_status, ds_status)
+VALUES('0','Inativo',false,'Usuário de sistema desativado.');
+
+INSERT INTO tb_status(co_status, nm_status, st_status, ds_status)
+VALUES('2','Suspenso',true,'Usuário de sistema com atividades supensas porem ativo.');
