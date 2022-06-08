18f622957634f4e0d5106a0391f45c2a2d6f900f
p000059 <52934496+p000059@users.noreply.github.com>
2022-06-08 13:14:12 -0300

    Update create_BDVACATION.sql

--- PostgreSQL/create_BDVACATION.sql
+++ PostgreSQL/create_BDVACATION.sql
@@ -5,7 +5,7 @@ CREATE TABLE type_persons(
 	nm_type_person varchar(70) NOT NULL,
 	ds_type_person varchar(100) NOT NULL,
 	st_type_person BIT(1) NOT NULL
-)
+);
 
 CREATE TABLE persons(
 	
@@ -17,7 +17,7 @@ CREATE TABLE persons(
 	type_person_id BIGINT NOT NULL,
 	
 	FOREIGN KEY (type_person_id) REFERENCES type_persons(id)
-)
+);
 
 CREATE TABLE vacations(
 	
@@ -25,15 +25,15 @@ CREATE TABLE vacations(
 	co_vacation varchar(8) UNIQUE NOT NULL,
 	tp_vacation varchar(20) NOT NULL,
 	rs_vacation varchar(15) NOT NULL
-)
+);
 
 CREATE TABLE rl_persons_vacations(
 	
 	person_id BIGINT NOT NULL,
 	vacation_id BIGINT NOT NULL,
 	initial_date VARCHAR(14) NOT NULL,
-	final_date VARCHAR(14) NOT NULL
+	final_date VARCHAR(14) NOT NULL,
 	
 	FOREIGN KEY (person_id) REFERENCES persons(id),
 	FOREIGN KEY (vacation_id) REFERENCES vacations(id)
-)
+);
