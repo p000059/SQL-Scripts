194399440e0730cbc7022c16675fc73ece41bbdc
p000059 <52934496+p000059@users.noreply.github.com>
2022-06-14 11:54:21 -0300

    Update create_BDVACATION.sql

--- PostgreSQL/create_BDVACATION.sql
+++ PostgreSQL/create_BDVACATION.sql
@@ -96,6 +96,26 @@ CREATE TABLE positions(
 	FOREIGN KEY (type_position_id) REFERENCES type_positions(id)
 );
 
+CREATE TABLE salaries(
+	
+	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_salary CHAR(4) NOT NULL,
+	ds_salary VARCHAR(150) NOT NULL,
+	vl_salary FLOAT NOT NULL,
+	st_salary BIT(1) NOT NULL
+);
+
+CREATE TABLE rl_positions_salaries(
+	
+	position_id BIGINT NOT NULL,
+	salary_id BIGINT NOT NULL,
+	dt_initial VARCHAR(14) NOT NULL,
+	dt_final VARCHAR(14) NOT NULL,
+
+	FOREING KEY (position_id) REFERENCES positions(id),
+	FOREING KEY (salary_id) REFERENCES salaries(id)
+)
+
 CREATE TABLE rl_persons_adresses(
 
 	person_id BIGINT NOT NULL,
@@ -155,6 +175,66 @@ VALUES ('02','PJ','Pessoa Jurídica',1);
 
 INSERT INTO type_persons(co_tperson, nm_tperson, ds_tperson, st_tperson)
 VALUES ('03','ES','Estrangeiro',1);
+
+INSERT INTO type_user(co_tuser, nm_tuser, ds_tuser, st_user)
+VALUES ('01', 'Empregado', 'Empregrado diretamente efetivado na empresa', 1);
+
+INSERT INTO type_user(co_tuser, nm_tuser, ds_tuser, st_user)
+VALUES ('02', 'Empregado Terceirizado', 'Empregrado prestador de serviço por terceirização', 1);
+
+INSERT INTO type_user(co_tuser, nm_tuser, ds_tuser, st_user)
+VALUES ('03', 'Estagiário', 'Estagiário diretamente contratado na empresa', 1);
+
+INSERT INTO type_user(co_tuser, nm_tuser, ds_tuser, st_user)
+VALUES ('04', 'Empregado Temporário', 'Empregrado temporariamente contratado para prestar serviços à empresa.', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Gerente', 'Gestor de um setor', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Coordenador', 'Responsável por organizar e ordenar uma área', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Coordenador Local', 'Organiza e ordena um setor de uma área', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Superintendente', 'Gestor de um setor', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Diretor', 'Gestor de um setor ou mais setores', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Assessor Local', 'Gestor de de parte de um setor e auxilia o gestor local.', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Assessor', 'Auxilia o gestor do setor.', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Assistente Técnico', 'Técnico da área negocial.', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Assistente Administrativo', 'Auxilia nas atividades operacionais da área', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Cobertura', 'Pessoa que cobre férias de outros funcionários', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Atendente', 'Responsável por controlar, informar e direcionar pessoas à empresa.', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Porteiro', 'Controla entrada e saída de veículos e pessoas.', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Copeiro(a)', 'Desempenha atividades de copa da empresa.', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Auxiliar de Serviços', 'Responsável por efetuar a limpeza, conservação e organização de empresa', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Encarregado de Serviços', 'Gestor do pessoal de Auxiliar de Serviços', 1);
+
+INSERT INTO type_positions(co_tposition, nm_tposition, ds_position, st_position)
+VALUES ('Técnico de manutenção', 'Responsável pela manutenção mecânica e elétrica predial.', 1);
 
 
 
