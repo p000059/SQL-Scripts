9dd9cffd5a4b06db016a7d810331606ee6380b53
p000059 <52934496+p000059@users.noreply.github.com>
2022-06-14 12:26:31 -0300

    Update create_BDVACATION.sql

--- PostgreSQL/create_BDVACATION.sql
+++ PostgreSQL/create_BDVACATION.sql
@@ -11,15 +11,15 @@ CREATE TABLE type_persons(
 	
 	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
 	co_tperson CHAR(2) UNIQUE NOT NULL,
-	nm_tperson VARCHAR(15) NOT NULL,
-	ds_tperson VARCHAR(25) NOT NULL,
+	nm_tperson VARCHAR(25) NOT NULL,
+	ds_tperson VARCHAR(50) NOT NULL,
 	st_tperson BIT(1) NOT NULL
 );
 
 CREATE TABLE type_positions(
 	
 	id SERIAL NOT NULL UNIQUE PRIMARY KEY,	
-	nm_tposition VARCHAR(15) NOT NULL,
+	nm_tposition VARCHAR(30) NOT NULL,
 	ds_tposition VARCHAR(150) NOT NULL,
 	st_tposition BIT(1) NOT NULL
 );
@@ -65,7 +65,7 @@ CREATE TABLE users(
 	
 	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
 	co_user CHAR(7) NOT NULL,
-  	nm_user VARCHAR(20)NOT NULL,
+  	nm_user VARCHAR(30)NOT NULL,
   	ds_user VARCHAR(50)NOT NULL,
 	dt_create CHAR(14) NOT NULL,
   	st_user BIT(1)NOT NULL,
@@ -78,7 +78,7 @@ CREATE TABLE vacations(
 	
 	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
 	co_vacation VARCHAR(10) UNIQUE NOT NULL,
-	tp_vacation VARCHAR(15) NOT NULL,
+	tp_vacation VARCHAR(25) NOT NULL,
 	dy_vacation CHAR(2) NOT NULL,
 	st_vacation BIT(1) NOT NULL
 );
@@ -187,51 +187,58 @@ VALUES ('03', 'Estagiário', 'Estagiário diretamente contratado na empresa', '1
 INSERT INTO type_users(co_tuser, nm_tuser, ds_tuser, st_tuser)
 VALUES ('04', 'Temporário', 'Empregrado temporário.', '1');
 
+INSERT INTO type_users(co_tuser, nm_tuser, ds_tuser, st_tuser)
+VALUES ('05', 'Colaborador', 'Empregrado sem vínculo com a empresa.', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_users(co_tuser, nm_tuser, ds_tuser, st_tuser)
+VALUES ('06', 'Servidor', 'Efetivo da empresa.', '1');
+
+
+
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Gerente', 'Gestor de um setor', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Coordenador', 'Responsável por organizar e ordenar uma área', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Coordenador Local', 'Organiza e ordena um setor de uma área', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Superintendente', 'Gestor de um setor', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Diretor', 'Gestor de um setor ou mais setores', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Assessor Local', 'Gestor de de parte de um setor e auxilia o gestor local.', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Assessor', 'Auxilia o gestor do setor.', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Assistente Técnico', 'Técnico da área negocial.', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Assistente Administrativo', 'Auxilia nas atividades operacionais da área', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Cobertura', 'Pessoa que cobre férias de outros funcionários', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Atendente', 'Responsável por controlar, informar e direcionar pessoas à empresa.', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Porteiro', 'Controla entrada e saída de veículos e pessoas.', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Copeiro(a)', 'Desempenha atividades de copa da empresa.', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Auxiliar de Serviços', 'Responsável por efetuar a limpeza, conservação e organização de empresa', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Encarregado de Serviços', 'Gestor do pessoal de Auxiliar de Serviços', '1');
 
-INSERT INTO type_positions(nm_tposition, ds_position, st_position)
+INSERT INTO type_positions(nm_tposition, ds_tposition, st_tposition)
 VALUES ('Técnico de manutenção', 'Responsável pela manutenção mecânica e elétrica predial.', '1');
