e393808883330b6376288e371fe1317672df26a2
p000059 <52934496+p000059@users.noreply.github.com>
2022-06-15 13:53:49 -0300

    20220615135300PM

---
+++ PostgreSQL/insert_person.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION fc_InsertPerson
+(
+	co_person CHAR(7), 
+	nm_person VARCHAR(100), 
+	ds_person VARCHAR(150), 
+	dt_birth CHAR(8),
+	st_person BIT(1),
+	sex_id INTEGER
+)
+RETURNS BOOLEAN
+AS $$
+BEGIN
+	INSERT INTO tb_Person VALUES($1, $2, $3, $4, $5, $6);
+	RETURN TRUE;
+END
+$$
+LANGUAGE plpgsql;
