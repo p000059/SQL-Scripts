49d3cefb30405fe59d6bb16f00a78cd1224eae69
p000059 <52934496+p000059@users.noreply.github.com>
2022-07-05 23:01:32 -0300

    CREATE DATA BASE
    Primeiro commit do script de criação do banco.

---
+++ PostgreSQL/PROJECTS/003/SQL/create_BDWORK.sql
@@ -0,0 +1,108 @@
+
+CREATE TABLE tb_sector (
+	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_sector INTEGER NOT NULL UNIQUE,
+	nm_sector VARCHAR(30) NOT NULL,
+	ds_sector VARCHAR(300) NOT NULL,
+	tp_sector VARCHAR(30) NOT NULL,
+	st_sector BIT NOT NULL
+);
+
+
+CREATE TABLE tb_vacation (
+	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_vacation BIGINT NOT NULL UNIQUE,
+	nm_vacation VARCHAR(30) NOT NULL,
+	ds_vacation VARCHAR(300) NOT NULL,
+	tp_vacation VARCHAR(30) NOT NULL,
+	nr_day INTEGER NOT NULL,
+	st_vacation BIT NOT NULL
+);
+
+
+CREATE TABLE tb_frequency (
+	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_frequency BIGINT NOT NULL UNIQUE,
+	nm_frequency VARCHAR(30) NOT NULL,
+	ds_frequency VARCHAR(300) NOT NULL,
+	tp_frequency VARCHAR(30) NOT NULL,
+	nr_day INTEGER NOT NULL,
+	st_frequency BIT NOT NULL
+);
+
+
+CREATE TABLE tb_salary (
+	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_salary INTEGER NOT NULL UNIQUE,
+	nm_salary VARCHAR(30) NOT NULL,
+	ds_salary VARCHAR(300) NOT NULL,
+	tp_salary VARCHAR(30) NOT NULL,
+	vl_salary REAL NOT NULL,
+	st_salary BIT NOT NULL
+);
+
+
+CREATE TABLE tb_position (
+	id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
+	co_position INTEGER NOT NULL UNIQUE,
+	nm_position VARCHAR(30) NOT NULL,
+	ds_position VARCHAR(300) NOT NULL,
+	tp_position VARCHAR(30) NOT NULL,
+	st_position BIT NOT NULL
+);
+
+
+CREATE TABLE rl_position_user (
+	id_user BIGINT NOT NULL,
+	id_position INTEGER NOT NULL,
+	dt_initial CHAR(14) NOT NULL,
+	dt_final CHAR(14) NOT NULL,
+	
+	FOREIGN KEY (id_position) REFERENCES tb_position (id)
+);
+
+
+CREATE TABLE rl_position_sector (
+	id_position INTEGER NOT NULL,
+	id_sector INTEGER NOT NULL,
+	dt_initial CHAR(14) NOT NULL,
+	dt_final CHAR(14),
+	
+	FOREIGN KEY (id_position) REFERENCES tb_position (id),
+	FOREIGN KEY (id_sector) REFERENCES tb_sector (id)
+);
+
+
+CREATE TABLE rl_position_salary (
+	id_position INTEGER NOT NULL,
+	id_salary BIGINT NOT NULL,
+	dt_initial CHAR(14) NOT NULL,
+	dt_final CHAR(14),
+	
+	FOREIGN KEY (id_position) REFERENCES tb_position (id),
+	FOREIGN KEY (id_salary) REFERENCES tb_salary (id)
+);
+
+
+CREATE TABLE rl_position_frequency (
+	id_position INTEGER NOT NULL,
+	id_frequency BIGINT NOT NULL,
+	dt_initial CHAR(14) NOT NULL,
+	dt_final CHAR(14),
+	
+	FOREIGN KEY (id_position) REFERENCES tb_position (id),
+	FOREIGN KEY (id_frequency) REFERENCES tb_frequency (id)
+);
+
+
+CREATE TABLE rl_position_vacation (
+	id_position INTEGER NOT NULL,
+	id_vacation BIGINT NOT NULL,
+	dt_initial CHAR(14) NOT NULL,
+	dt_final CHAR(14),
+	
+	FOREIGN KEY (id_position) REFERENCES tb_position (id),
+	FOREIGN KEY (id_vacation) REFERENCES tb_vacation (id)
+);
+
+
