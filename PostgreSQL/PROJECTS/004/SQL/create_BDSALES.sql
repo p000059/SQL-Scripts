a8a2e6637a28cc16f1c0995ade21ba254fa37ff7
p000059 <52934496+p000059@users.noreply.github.com>
2022-07-05 23:35:59 -0300

    CREATE BDSALES
    Script de criação do banco de dados BDSALES, para controle de vendas.

---
+++ PostgreSQL/PROJECTS/004/SQL/create_BDSALES.sql
@@ -0,0 +1,69 @@
+
+CREATE TABLE tb_value (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	tx_value FLOAT NOT NULL,
+	vl_tax FLOAT NOT NULL,
+	tt_value FLOAT NOT NULL,
+	dc_value FLOAT NOT NULL,
+	fn_value FLOAT NOT NULL,
+	st_value BIT NOT NULL
+);
+
+
+CREATE TABLE tb_payment (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	co_payment BIGSERIAL NOT NULL UNIQUE,
+	nm_payment VARCHAR(30) NOT NULL,
+	ds_pagament VARCHAR(300) NOT NULL,
+	tp_payment VARCHAR(30) NOT NULL,
+	nr_payment_installment INTEGER NOT NULL,
+	vl_payment FLOAT NOT NULL,
+	st_payment BIT NOT NULL
+);
+
+
+CREATE TABLE tb_sale (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	co_sale BIGSERIAL NOT NULL UNIQUE,
+	nm_sale VARCHAR(30) NOT NULL,
+	ds_sale VARCHAR(300) NOT NULL,
+	tp_sale VARCHAR(30) NOT NULL,
+	qt_sale_product INTEGER NOT NULL,
+	id_value BIGINT NOT NULL,
+	id_payment BIGINT NOT NULL,
+	st_sale BIGINT NOT NULL,
+	
+	FOREIGN KEY (id_value) REFERENCES tb_value (id),
+	FOREIGN KEY (id_payment) REFERENCES tb_payment (id)
+);
+
+
+CREATE TABLE rl_sale_person (
+	id_person INTEGER NOT NULL,
+	id_sale BIGINT NOT NULL,
+	dt_initial CHAR(14) NOT NULL,
+	dt_final CHAR(14) NOT NULL,
+	
+	FOREIGN KEY (id_sale) REFERENCES tb_sale (id)
+);
+
+
+CREATE TABLE rl_sale_user (
+	id_user BIGINT NOT NULL,
+	id_sale BIGINT NOT NULL,
+	dt_initial CHAR(14) NOT NULL,
+	dt_final CHAR NOT NULL,
+	
+	FOREIGN KEY (id_sale) REFERENCES tb_sale (id)
+);
+
+
+CREATE TABLE rl_product_sale (
+	id_product BIGINT NOT NULL,
+	id_sale BIGINT NOT NULL,
+	dt_initial CHAR(14) NOT NULL,
+	dt_final CHAR(14),
+	
+	FOREIGN KEY (id_sale) REFERENCES tb_sale (id)
+);
+
